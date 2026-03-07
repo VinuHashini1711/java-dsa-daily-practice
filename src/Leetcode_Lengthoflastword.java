a67fe39a16f61713e207b2dafb7ce9ac5a7f651e
VinuHashini1711 <vinuhashinivenkatesh17@gmail.com>
2026-03-07 22:27:32 +0530

    Day 3

---
+++ src/Leetcode_Lengthoflastword.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Leetcode_Lengthoflastword {
+    public static int lengthOfLastWord(String s) {
+        int end = s.length() - 1;
+        // Skip trailing spaces
+        while (end >= 0 && s.charAt(end) == ' ') {
+            end--;
+        }
+        int start = end;
+        // Move until space or beginning
+        while (start >= 0 && s.charAt(start) != ' ') {
+            start--;
+        }
+        return end - start;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String s = sc.nextLine();
+        int result = lengthOfLastWord(s);
+        System.out.println("Length of last word: " + result);
+        sc.close();
+    }
+}
