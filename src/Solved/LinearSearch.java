d0439d8abad47025e556f77168731ae8dd8d0566
VinuHashini1711 <vinuhashinivenkatesh17@gmail.com>
2026-03-16 09:39:18 +0530

    Day 10

--- src/Solved/LinearSearch.java
+++ src/Solved/LinearSearch.java
@@ -4,10 +4,8 @@ class LinearSearch {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();      // size of array
         int[] arr = new int[n];
-
         // read array elements
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
