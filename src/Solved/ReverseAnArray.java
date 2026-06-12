d6bdda026d1c59f537b2c19cbe1675f6149b0226
VinuHashini1711 <vinuhashinivenkatesh17@gmail.com>
2026-06-12 20:30:59 +0530

    Updated ReverseAnArray

--- src/Solved/ReverseAnArray.java
+++ src/Solved/ReverseAnArray.java
@@ -8,11 +8,19 @@ public static void main(String[] args) {
     for(int i=0;i<n;i++){
         arr[i]=sc.nextInt();
     }
-    for(int i=0;i<n/2;i++){
-        int temp=arr[i];
-        arr[i]=arr[n-1-i];
-        arr[n-1-i]=temp;
+//    for(int i=0;i<n/2;i++){
+//        int temp=arr[i];
+//        arr[i]=arr[n-1-i];
+//        arr[n-1-i]=temp;
+//    }
+//    System.out.print(Arrays.toString(arr));
+    System.out.print("[");
+    for(int i=n-1;i>=0;i--){
+        System.out.print(arr[i]);
+        if(i!=0){
+            System.out.print(", ");
+        }
     }
-    System.out.print(Arrays.toString(arr));
+    System.out.print("]");
 }
 }
