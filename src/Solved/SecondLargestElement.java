aa38b69b1ac8544e618b84ca762b728872f42fd4
VinuHashini1711 <vinuhashinivenkatesh17@gmail.com>
2026-06-13 19:09:48 +0530

    Second Largest element

---
+++ src/Solved/SecondLargestElement.java
@@ -0,0 +1,21 @@
+package Solved;
+import java.util.*;
+public class SecondLargestElement {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++) {
+            arr[i] = sc.nextInt();
+        }
+            Arrays.sort(arr);
+            int largest=arr[n-1];
+            for(int i=n-2;i>=0;i--){
+                if(arr[i]!=largest){
+                    System.out.print(arr[i]);
+                    return;
+                }
+            }
+            System.out.print(-1);
+        }
+    }
