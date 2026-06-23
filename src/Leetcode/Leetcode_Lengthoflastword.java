cead51fb77f2d378f951a65fad6ae18562a2308f
VinuHashini1711 <vinuhashinivenkatesh17@gmail.com>
2026-06-23 22:58:34 +0530

    Cyclic sort

--- src/Leetcode/Leetcode_Lengthoflastword.java
+++ src/Leetcode/Leetcode_Lengthoflastword.java
@@ -1,21 +1,20 @@
 package Leetcode;
-
 import java.util.Scanner;
-
 public class Leetcode_Lengthoflastword {
     public static int lengthOfLastWord(String s) {
         int end = s.length() - 1;
         // Skip trailing space
-        while (end >= 0 && s.charAt(end) == ' ') {
+        while (end >= 0 && s.charAt(end) == ' '){
             end--;
         }
         int start = end;
         // Move until space or beginning
-        while (start >= 0 && s.charAt(start) != ' ') {
+        while (start >= 0 && s.charAt(start) != ' '){
             start--;
         }
         return end - start;
     }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a sentence:");
--- src/Solved/BinarySearch.java
+++ src/Solved/BinarySearch.java
@@ -2,26 +2,18 @@ package Solved;
 import java.util.*;
 class BinarySearch {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();     // size of array
         int[] arr = new int[n];
-
         for(int i = 0; i < n; i++){
             arr[i] = sc.nextInt();
         }
-
         int target = sc.nextInt();
-
         int left = 0;
         int right = n - 1;
         int result = -1;
-
         while(left <= right){
-
             int mid = (left + right) / 2;
-
             if(arr[mid] == target){
                 result = mid;
                 break;
@@ -33,7 +25,6 @@ class BinarySearch {
                 right = mid - 1;
             }
         }
-
         System.out.println(result);
     }
 }
