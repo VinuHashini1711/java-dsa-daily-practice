9e50a9c6221a6c45c2b22d9ede8197dc9bbb3c1c
VinuHashini1711 <vinuhashinivenkatesh17@gmail.com>
2026-08-10 20:11:19 +0530

    Abstract Class

---
+++ src/oops/AbstractClass.java
@@ -0,0 +1,60 @@
+package oops;
+
+public class AbstractClass {
+
+    static abstract class Vehicle {
+
+        int speed;
+
+        Vehicle(int speed) {
+            this.speed = speed;
+        }
+
+        void start() {
+            System.out.println("Vehicle started");
+        }
+
+        void stop() {
+            System.out.println("Vehicle stopped");
+        }
+
+        abstract void move();
+    }
+
+    static class Car extends Vehicle {
+
+        Car(int speed) {
+            super(speed);
+        }
+
+        @Override
+        void move() {
+            System.out.println("Car is moving at " + speed + " km/h");
+        }
+    }
+
+    static class Bike extends Vehicle {
+
+        Bike(int speed) {
+            super(speed);
+        }
+
+        @Override
+        void move() {
+            System.out.println("Bike is moving at " + speed + " km/h");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Car car = new Car(100);
+        car.start();
+        car.move();
+        car.stop();
+
+        Bike bike = new Bike(60);
+        bike.start();
+        bike.move();
+        bike.stop();
+    }
+}
