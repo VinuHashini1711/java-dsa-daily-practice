7fa3445c2634b790c7fe37eec1b00e4e8e851595
VinuHashini1711 <vinuhashinivenkatesh17@gmail.com>
2026-08-12 23:54:57 +0530

    Inheritance

---
+++ src/oops/Inheritance.java
@@ -0,0 +1,22 @@
+package oops;
+
+public class Inheritance {
+    class Animal {
+        void eat() {
+            System.out.println("Eating");
+        }
+    }
+
+     static class Dog extends Animal {
+        void bark() {
+            System.out.println("Barking");
+        }
+    }
+
+        public static void main(String[] args) {
+            Dog d = new Dog();
+
+            d.eat();   // inherited method
+            d.bark();  // own method
+}
+}
