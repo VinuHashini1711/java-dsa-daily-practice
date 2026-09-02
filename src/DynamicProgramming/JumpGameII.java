1fcf23db76521f74f9d8410c36db7b656096e602
VinuHashini1711 <vinuhashinivenkatesh17@gmail.com>
2026-09-02 23:14:13 +0530

    Updated

--- src/DynamicProgramming/JumpGameII.java
+++ src/DynamicProgramming/JumpGameII.java
@@ -18,15 +18,11 @@ public class JumpGameII {
 }
 
 class Solution {
-
     public int jump(int[] nums) {
-
         int farthest = 0;
         int jumps = 0;
         int currEnd = 0;
-
         for (int i = 0; i < nums.length - 1; i++) {
-
             farthest = Math.max(farthest, i + nums[i]);
 
             if (i == currEnd) {
