9797519c4ab2590abaaa8adb747d20c007efd667
VinuHashini1711 <vinuhashinivenkatesh17@gmail.com>
2026-09-03 21:52:01 +0530

    Decode Ways3

---
+++ src/DynamicProgramming/DecodeWays3.java
@@ -0,0 +1,45 @@
+package DynamicProgramming;
+
+public class DecodeWays3 {
+
+    public static int numDecodings(String s) {
+
+        int n = s.length();
+
+        if (n == 0 || s.charAt(0) == '0') {
+            return 0;
+        }
+
+        int prev2 = 1;
+        int prev1 = 1;
+
+        for (int i = 1; i < n; i++) {
+
+            int current = 0;
+
+            // One digit
+            if (s.charAt(i) != '0') {
+                current += prev1;
+            }
+
+            // Two digits
+            int twoDigit = Integer.parseInt(s.substring(i - 1, i + 1));
+
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                current += prev2;
+            }
+
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "226";
+
+        System.out.println(numDecodings(s));
+    }
+}
