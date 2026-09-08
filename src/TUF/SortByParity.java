2ce9df5873e180ece3b79b867dcc3951b73d496a
VinuHashini1711 <vinuhashinivenkatesh17@gmail.com>
2026-09-08 20:46:02 +0530

    Sort By Parity

---
+++ src/TUF/SortByParity.java
@@ -0,0 +1,31 @@
+package TUF;
+import java.util.Arrays;
+public class SortByParity {
+    public static int[] sortArrayByParity(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] % 2 == 0) {
+                left++;
+            }
+            else if (nums[right] % 2 != 0) {
+                right--;
+            }
+            else {
+                // left = odd, right = even
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+        return nums;
+    }
+    public static void main(String[] args) {
+        int[] nums = {3, 1, 2, 4};
+        int[] result = sortArrayByParity(nums);
+        System.out.println(Arrays.toString(result));
+    }
+}
