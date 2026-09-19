b11130fcc9ccb6a44e5c7825b283d3a797fa9979
VinuHashini1711 <vinuhashinivenkatesh17@gmail.com>
2026-09-19 21:18:01 +0530

    cyclic sort

--- src/DSA/CyclicSort.java
+++ src/DSA/CyclicSort.java
@@ -3,9 +3,7 @@ import java.util.Arrays;
 public class CyclicSort {
         public static void main(String[] args) {
             int[] arr = {3, 5, 2, 1, 4};
-
             cyclicSort(arr);
-
             System.out.println(Arrays.toString(arr));
         }
         static void cyclicSort(int[] arr) {
