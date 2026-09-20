b1f4a0e121ff81c15a9b222c3ae7f760822e5e33
VinuHashini1711 <vinuhashinivenkatesh17@gmail.com>
2026-09-20 17:39:54 +0530

    matrix diagonal sum

--- src/TCS_NQT/Matrix/MatrixDiagonalSum.java
+++ src/TCS_NQT/Matrix/MatrixDiagonalSum.java
@@ -10,6 +10,7 @@ public class MatrixDiagonalSum {
                 arr[i][j]=sc.nextInt();
             }
         }
+
         int sum=0;
         for(int i=0;i<n;i++){
             sum+=arr[i][i];
