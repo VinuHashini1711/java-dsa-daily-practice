f701e5674cad92a4059c7745f199ee046ca74bef
VinuHashini1711 <vinuhashinivenkatesh17@gmail.com>
2026-09-21 19:30:15 +0530

    andy's updated code

--- src/DynamicProgramming/AndysVacation.java
+++ src/DynamicProgramming/AndysVacation.java
@@ -21,11 +21,13 @@ public class AndysVacation {
         dp[0][0]=0;
         dp[0][1]=contest[0];
         dp[0][2]=gym[0];
+
         for(int i=1;i<n;i++){
             dp[i][0]=Math.max(dp[i-1][0],Math.max(dp[i-1][1],dp[i-1][2]));
             dp[i][1]=contest[i]+Math.max(dp[i-1][0],dp[i-1][2]);
             dp[i][2]=gym[i]+Math.max(dp[i-1][0],dp[i-1][1]);
         }
+
         int answer=Math.max(dp[n-1][0],Math.max(dp[n-1][1],dp[n-1][2]));
         System.out.print(answer);
     }
